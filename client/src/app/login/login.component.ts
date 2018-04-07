9ea9d1cbdefe837d0585bdf93ff865d4aca9d2cb
David Gonzalez <davidgg666@gmail.com>
2018-04-07 13:25:43 +0200

    Change login component to use user service

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,24 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { WebSocketService } from '../websocket.service';
+import { Subscription } from 'rxjs/Subscription';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  private socketSubscription: Subscription;
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
-  ngOnInit() {
-  }
 
   login(user: string, pass: string) {
-    console.log(user, pass);
+    this.userService.login(user, pass);
   }
 
-  register() {
-    console.log('Registar..');
+  register(user: string, pass: string) {
+    this.userService.login(user, pass);
   }
 
 }
