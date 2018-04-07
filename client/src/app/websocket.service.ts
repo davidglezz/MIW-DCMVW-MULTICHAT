65039eadf4678ee81d981c7a1eea775225b0ab1c
David Gonzalez <davidgg666@gmail.com>
2018-04-07 13:26:55 +0200

    Add topics

--- client/src/app/websocket.service.ts
+++ client/src/app/websocket.service.ts
@@ -1,39 +1,77 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+import { Subject } from 'rxjs/Subject';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/share';
 import 'rxjs/add/operator/retryWhen';
 import 'rxjs/add/operator/delay';
 import { QueueingSubject } from 'queueing-subject'
 import websocketConnect from 'rxjs-websockets'
 
+import { Command } from './models/Command';
+
 @Injectable()
 export class WebSocketService {
-  private inputStream: QueueingSubject<string>
-  public messages: Observable<object>
+  private outgoing: QueueingSubject<string>
+  private incoming: Subscription
   public connectionStatus: Observable<number>
+  private pingTimer: number
+  // Clasifico los mensajes entrantes en múltiples Observables
+  private topics: { [key: string]: Subject<Command> } = {}
 
   private static readonly URL = "ws://127.0.0.1/" //window.location.host;
 
   constructor() {
-    //this.connect();
+    this.getTopic('none').subscribe(message => {
+      console.log('Mensaje de texto recibido:', message)
+    })
+    this.getTopic('notfound').subscribe(message => {
+      console.error('No existe un manejador para el mensaje:', message)
+    })
+
+    this.connect();
   }
 
   public connect() {
-    if (this.messages)
+    if (this.incoming)
       return
 
-    this.inputStream = new QueueingSubject<string>()
-    const { connectionStatus, messages } = websocketConnect(WebSocketService.URL, this.inputStream)
+    this.outgoing = new QueueingSubject<string>() // Cola de salida de mensajes
+    const { connectionStatus, messages } = websocketConnect(WebSocketService.URL, this.outgoing)
     this.connectionStatus = connectionStatus
-    this.messages = messages.retryWhen(errors => errors.delay(10000))
-        .map(message => message === 'pong' ? message : JSON.parse(message))
-        .share();
+    this.incoming = messages
+      .retryWhen(errors => errors.delay(10000))
+      .map(message => {
+        if (message[0] === '{') { // or message !== 'pong
+          try {
+            return JSON.parse(message)
+          } catch (err) {
+            console.error('No es json:', message)
+          }
+        }
+        return {topic: 'none', fn:message};
+      })
+      .subscribe((message: Command) => {
+        if (message && message.topic && this.topics[message.topic]) {
+          this.topics[message.topic].next(message)
+        } else {
+          this.topics['notfound'].next(message)
+        }
+      })
 
-    setInterval(() => this.inputStream.next('ping'), 30000);//this.socketService.send('ping');
+    this.pingTimer = window.setInterval(() => this.outgoing.next('ping'), 30000);
   }
 
-  public send(obj: object):void {
-    this.inputStream.next(JSON.stringify(obj))
+  public getTopic(topic: string): Subject<Command> {
+    if (!this.topics[topic]) {
+      this.topics[topic] = new Subject()
+    }
+    return this.topics[topic]
+  }
+
+  public send(obj: Command): void {
+    this.outgoing.next(JSON.stringify(obj))
   }
 }
