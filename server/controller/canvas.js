a611352edc655ff82ce7c09646f547292a2b9dac
David Gonzalez <davidgg666@gmail.com>
2018-04-09 01:17:51 +0200

    Add canvas controller

---
+++ server/controller/canvas.js
@@ -0,0 +1,9 @@
+const User = require('../persistence/user');
+
+const fabric = require('fabric').fabric;
+// const canvas = new fabric.StaticCanvas();
+// https://github.com/kangax/fabric.js/issues/4586
+
+module.exports = (function userFunctions() {
+  return { }
+})()
