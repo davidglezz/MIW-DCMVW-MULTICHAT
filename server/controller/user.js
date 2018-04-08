d1c67812b5e9bb5e3b17b03b70f2ffba3148b331
David Gonzalez <davidgg666@gmail.com>
2018-04-09 01:37:47 +0200

    Improve user controller

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,63 +1,55 @@
 const User = require('../persistence/user');
+const Command = require('../model/command');
 
 module.exports = (function userFunctions() {
+
+  function getUserList(wss) {
+    return [...wss.clients]
+      .filter(c => c && c.user)
+      .map(c => ({id: c.id, name:c.user.username}))
+  }
 
   async function login(username, password) {
     try {
       var user = await User.findOne({ username, password });
     } catch (ex) {
-      return {
-        topic: 'alert',
-        fn: 'showMessage',
-        args: ['Ocurrió un error. Intentalo de nuevo.']
-      }
+      return new Command('alert', 'showMessage', ['Ocurrió un error. Intentalo de nuevo.'])
     }
 
     if (!user) {
-      return {
-        topic: 'alert',
-        fn: 'showMessage',
-        args: ['Usuario o contraseña incorrectos.']
-      }
+      return new Command('alert', 'showMessage', ['Usuario o contraseña incorrectos.'])
     }
 
     this.id = user._id
     this.user = user
-
-    this.server.broadcast({
-      topic: 'user',
-      fn: 'userConnect',
-      args: [this.id, username]
-    })
-    return {
-      topic: 'user',
-      fn: 'loggedin',
-      args: [this.id, username]
-    }
+    this.server.broadcast(new Command('user', 'userConnect', [this.id, username]))
+    return new Command('user', 'loggedin', [this.id, username, getUserList(this.server)])  
   }
 
   async function register(username, password) {
-    var newUser = User({ username, password })
+    var user = User({ username, password })
     try {
-      await newUser.save()
+      const res = await user.save()
+      console.log(res);
     } catch (ex) {
-      console.error(ex.message)
-      return {
-        topic: 'alert',
-        fn: 'showMessage',
-        args: ['Ocurrió un error, puede que el usuario ya exista.']
-      }
+      // console.error(ex.message)
+      return new Command('alert', 'showMessage', ['Ocurrió un error, puede que el usuario ya exista.'])
     }
-    this.server.broadcast({
-      topic: 'user',
-      fn: 'newUser',
-      args: [this.id, username]
-    })
-    return {
-      topic: 'user',
-      fn: 'loggedin',
-      args: [this.id, username]
-    }
+    // Login
+    this.id = user._id
+    this.user = user
+    this.server.broadcast(new Command('user', 'newUser', [this.id, username]))
+    return new Command('user', 'loggedin', [this.id, username, getUserList(this.server)]) 
+  }
+
+  async function logout(id) {
+    this.server.broadcast(new Command('user', 'userDisconnect', [this.id, this.user.username]))
+    this.user = undefined
+    return new Command('user', 'requestAuth', [])
+  }
+
+  async function userlist() {
+    return new Command('user', 'updateUserList', [getUserList(this.server)])
   }
   
   return { login, register }
