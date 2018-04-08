8f135d6c9c368aabf6cd10158e1b851549827c7d
David Gonzalez <davidgg666@gmail.com>
2018-04-08 18:38:35 +0200

    improve chat-room component

--- client/src/app/chat-page/chat-page.component.ts
+++ client/src/app/chat-page/chat-page.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-chat-page',
   templateUrl: './chat-page.component.html',
   styleUrls: ['./chat-page.component.css']
 })
-export class ChatPageComponent implements OnInit {
+export class ChatPageComponent implements OnInit, OnDestroy {
 
   constructor() { }
 
   ngOnInit() {
+
   }
 
+  ngOnDestroy() {
+    console.log('Se salió de la sala de chat principal')
+  }
 }
