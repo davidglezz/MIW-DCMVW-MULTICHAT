4486c06e3506a313feafb9f1d4b279c6dbfb0c1e
David Gonzalez <davidgg666@gmail.com>
2018-04-08 18:37:33 +0200

    add p2p-chat empty component

---
+++ client/src/app/p2p-chat/p2p-chat.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-p2p-chat',
+  templateUrl: './p2p-chat.component.html',
+  styleUrls: ['./p2p-chat.component.css']
+})
+export class P2pChatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
