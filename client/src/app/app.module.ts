d83105ac3d5579796656253a08a279e3cdb9acae
David Gonzalez <davidgg666@gmail.com>
2018-04-08 18:38:50 +0200

    Add some dependencies

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,19 +3,25 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatToolbarModule, MatButtonModule, MatCheckboxModule, MatInputModule, MatCardModule, MatSlideToggleModule, MatSnackBarModule } from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatCheckboxModule, MatInputModule, MatCardModule, MatSlideToggleModule, MatSnackBarModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ChatPageComponent } from './chat-page/chat-page.component';
 import { LoginComponent } from './login/login.component';
 import { WebSocketService } from './websocket.service';
 import { UserService } from './user.service';
+import { P2pChatComponent } from './p2p-chat/p2p-chat.component';
+import { RouteReuseStrategy } from '@angular/router';
+import { CustomReuseStrategy } from './CustomReuseStrategy';
+import { CanvasComponent } from './canvas/canvas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ChatPageComponent,
-    LoginComponent
+    LoginComponent,
+    P2pChatComponent,
+    CanvasComponent
   ],
   imports: [
     BrowserModule,
@@ -29,9 +35,12 @@ import { UserService } from './user.service';
     MatInputModule,
     MatCardModule,
     MatSlideToggleModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
-  providers: [WebSocketService, UserService],
+  providers: [WebSocketService, UserService, { provide: RouteReuseStrategy, useClass: CustomReuseStrategy }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
