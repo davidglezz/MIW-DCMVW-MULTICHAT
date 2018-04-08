2641e9ec59da9418e313bb9e92092a0b9ffca979
David Gonzalez <davidgg666@gmail.com>
2018-04-09 01:18:45 +0200

    Update routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ChatPageComponent } from './chat-page/chat-page.component';
 import { LoginComponent } from './login/login.component';
+import { P2pChatComponent } from './p2p-chat/p2p-chat.component';
+import { CanvasComponent } from './canvas/canvas.component';
 
 const routes: Routes = [
   {
@@ -11,6 +13,14 @@ const routes: Routes = [
   {
     path: 'chat',
     component: ChatPageComponent
+  },
+  {
+    path: 'canvas',
+    component: CanvasComponent
+  },
+  {
+    path: 'p2p-chat/:user',
+    component: P2pChatComponent
   },
   {
     path: '**',
