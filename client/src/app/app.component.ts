16cf45eb32dd08a5050d9b4a38955c91017b11a3
David Gonzalez <davidgg666@gmail.com>
2018-04-09 01:23:20 +0200

    Improve main page + sidenav

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { MatSnackBar } from '@angular/material';
 import { WebSocketService } from './websocket.service';
 import { Subscription } from 'rxjs/Subscription';
 import { Command } from './models/Command';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,7 @@ import { Command } from './models/Command';
 })
 export class AppComponent implements OnDestroy {
   private socketSubscription: Subscription;
-  constructor(private webSocketService: WebSocketService, private snackBar: MatSnackBar) {
+  constructor(private webSocketService: WebSocketService, private snackBar: MatSnackBar, private userService: UserService) {
     this.webSocketService.connect()
     this.socketSubscription = this.webSocketService.getTopic('alert').subscribe((message: Command) => {
       if (this[message.fn])
