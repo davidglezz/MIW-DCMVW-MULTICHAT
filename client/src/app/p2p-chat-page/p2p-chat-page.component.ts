86ad61be0348427fe11528f7468a6b60462235af
David Gonzalez <davidgg666@gmail.com>
2018-06-17 19:21:03 +0200

    Refactor user sevice and p2pchat

--- client/src/app/p2p-chat-page/p2p-chat-page.component.ts
+++ client/src/app/p2p-chat-page/p2p-chat-page.component.ts
@@ -1,11 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs/Subscription';
-import { WebSocketService } from '../websocket.service';
-import { Command } from '../models/Command';
 import { UserService } from '../user.service';
+import { WebSocketService } from '../websocket.service';
+import { P2pChatService } from '../p2p-chat.service';
 import { ChatMessage } from '../models/ChatMessage';
-
 
 /**
  * Debido a que la API RTCDataChannel (https://developer.mozilla.org/en-US/docs/Web/API/RTCDataChannel)
@@ -16,195 +14,26 @@ import { ChatMessage } from '../models/ChatMessage';
   templateUrl: './p2p-chat-page.component.html',
   styleUrls: ['./p2p-chat-page.component.css']
 })
-export class P2pChatPageComponent implements OnInit {
-  private userSubscription: Subscription;
-  private p2pchatSubscription: Subscription;
+export class P2pChatPageComponent {
   messages: ChatMessage[] = []
-  remoteUser: String
+  remoteUser: string
 
-  // WebRTC
-  localPeerConnection: RTCPeerConnection
-  remotePeerConnection: RTCPeerConnection
-  remoteaudio: HTMLAudioElement
-  stream: MediaStream | void
-
-  constructor(private webSocketService: WebSocketService, private userService: UserService, private route: ActivatedRoute) {
+  constructor(private p2pChatService: P2pChatService, private route: ActivatedRoute) {
     this.route.params.subscribe(params => {
       this.remoteUser = params['user']
+      this.messages = p2pChatService.getMessagesOf(this.remoteUser)
       console.log('Remote user: ' + this.remoteUser)
-    })//.unsubscribe()
-
-    this.webSocketService.connect()
-    this.userSubscription = this.webSocketService.getTopic('user').subscribe((message: Command) => {
-      if (this[message.fn])
-        this[message.fn].apply(this, message.args)
     })
-    this.p2pchatSubscription = this.webSocketService.getTopic('p2pchat').subscribe((message: Command) => {
-      if (this[message.fn])
-        this[message.fn].apply(this, message.args)
-    })
-
-    // WebRTC
-    if (typeof RTCPeerConnection === "undefined")
-      RTCPeerConnection = webkitRTCPeerConnection;
-  }
-  
-  message(name, text) {
-    if (name === this.remoteUser)
-      this.messages.push({type: 'text', name, text})
-  }
-
-  notify(name, text) {
-    if (name === this.remoteUser)
-      this.messages.push({type: 'notify', text})
-  }
-
-  sendMessage(text) {
-    if (text) {
-      this.messages.push({
-        type: 'text', 
-        name: this.userService.currentUser.name, 
-        text: text
-      })
-      this.webSocketService.send({
-        topic: 'p2pchat',
-        fn: 'message',
-        args: [this.remoteUser, text]
-      })
-    }
-  }
-
-  private requestAuth(id: string, name: string) {
-    this.messages = [];
-  }
-
-  private userConnect(id: string, name: string) {
-    if (name === this.remoteUser)
-      this.notify(name, name + ' se ha conectado.');
-  }
-
-  private userDisconnect(id: string, name: string) {
-    if (name === this.remoteUser)
-      this.notify(name, name + ' se ha desconectado.');
-  }
-
-  ngOnDestroy() {
-    this.userSubscription.unsubscribe()
-    this.p2pchatSubscription.unsubscribe()
-    console.log('Salió del chat con ' + this.remoteUser)
-  }
-
-  // WebRTC
-  async ngOnInit() {
-    console.log('ngOnInit')
-    this.listdevices()
   }
 
   call() {
     console.info("Call..")
   }
 
-  async listdevices() {
-    // List cameras and microphones.
-    const devices = await navigator.mediaDevices.enumerateDevices().catch(console.error)
-    if (devices) {
-      devices.forEach(device => console.log(device.kind + ": " + device.label + " id = " + device.deviceId));
+  sendMessage(text) {
+    if (text) {
+      this.p2pChatService.sendMessage(this.remoteUser, text)
     }
   }
-
-  async start() {
-    console.log("Requesting stream");
-    this.stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false }).catch(console.error)
-    console.log(this.stream)
-    // video.src = window.URL.createObjectURL(stream);
-    // video.play();
-  }
-
-  public startingCallCommunication() {
-    if (!this.stream)
-      return
-
-    const configuration: RTCConfiguration = { "iceServers": [{ "urls": "stun:stun.phoneserve.com" }] } // antes era "url"
-    this.localPeerConnection = new RTCPeerConnection(configuration);
-    console.log("Created local peer connection object");
-    this.localPeerConnection.addStream(this.stream);
-    console.log("Added localStream to localPeerConnection");
-    //Add a handler associated with ICE protocol events
-    this.localPeerConnection.onicecandidate = this.gotLocalIceCandidate.bind(this);
-    //...and a second handler to be activated as soon as the remote stream becomes available
-    this.localPeerConnection.onaddstream = this.gotRemoteStream.bind(this);
-  }
-
-  stop() {
-    if (this.stream) {
-      this.stream.getTracks().forEach(track => track.stop());
-    }
-  }
-
-  call2() {
-    if (!this.stream)
-      return
-    //This is an optional configuration string, associated with NAT traversal setup
-    const configuration = null;
-    this.localPeerConnection = new RTCPeerConnection(configuration);
-    this.localPeerConnection.onicecandidate = this.gotLocalIceCandidate.bind(this);
-
-    this.remotePeerConnection.onaddstream = this.gotRemoteStream.bind(this);
-    console.log("Created local and remote peer connection objects");
-    this.localPeerConnection.addStream(this.stream);
-    console.log("Added localStream to localPeerConnection");
-    //this.localPeerConnection.createOffer(this.gotLocalDescription.bind(this), this.onSignalingError.bind(this));
-  }
-
-  public hangup() {
-    console.log("Ending call");
-    this.localPeerConnection.close();
-    this.localPeerConnection = null;
-    this.webSocketService.send(new Command('p2pchat', 'leave', []))
-  }
-
-  /****** Server messages hendlers ******/
-
-  private onOffer(offer, source) {
-    this.startingCallCommunication();
-    //this.remoteUsername = source;
-    this.localPeerConnection.setRemoteDescription(new RTCSessionDescription(offer));
-    /*this.localPeerConnection.createAnswer(answer => {
-      this.localPeerConnection.setLocalDescription(answer);
-      this.webSocketService.send(new Command('p2pchat', 'answer', [answer]))
-    }, console.error);*/
-  }
-
-  private onAnswer(answer) {
-    this.localPeerConnection.setRemoteDescription(new RTCSessionDescription(answer));
-  };
-
-  private onCandidate(candidate) {
-    this.localPeerConnection.addIceCandidate(new RTCIceCandidate(candidate));
-  };
-
-
-  /****** Events hendlers ******/
-
-  //Handler to be called as soon as the remote stream becomes available
-  private gotRemoteStream(event) {
-    this.remoteaudio.src = window.URL.createObjectURL(event.stream);
-    this.remoteaudio.play();
-  }
-
-  private gotLocalDescription(description) {
-    this.webSocketService.send(new Command('p2pchat', 'offer', [description]))
-    this.localPeerConnection.setLocalDescription(description);
-  }
-
-  private gotLocalIceCandidate(event) {
-    if (event.candidate) {
-      this.webSocketService.send(new Command('p2pchat', 'candidate', [event.candidate]))
-    }
-  }
-
-  private onSignalingError(error) {
-    console.log('Failed to create signaling message : ' + error.name);
-  }
-
+  
 }
--- client/src/app/user.service.ts
+++ client/src/app/user.service.ts
@@ -3,62 +3,39 @@ import { WebSocketService } from './websocket.service';
 import { Subscription } from 'rxjs/Subscription';
 import { Command } from './models/Command';
 import { Router } from '@angular/router';
-import { User } from './models/User';
+import { User, UserStatus, NewUser } from './models/User';
 import { AppStorageService } from './AppStorage.service';
 
 @Injectable()
 export class UserService implements OnDestroy {
-  public isLoggedIn = false
-  public allUsers: User[] = []
+  public isLoggedIn = false // TODO poner private
   public currentUser: User
-  private socketSubscription: Subscription;
+  private userSubscription: Subscription;
 
   constructor(private webSocketService: WebSocketService, private router: Router, private appStorage: AppStorageService) {
-    this.users = appStorage.users;
-    this.webSocketService.connect()
-    this.socketSubscription = this.webSocketService.getTopic('user').subscribe((message: Command) => {
+    this.userSubscription = this.webSocketService.getTopic('user').subscribe((message: Command) => {
       if (this[message.fn])
         this[message.fn].apply(this, message.args)
     })
   }
 
-  public getUsers(withCurrent = true) {
-    return withCurrent ? this.allUsers : this.allUsers.filter(u => u.id != this.currentUser.id)
-  }
-
   public login(user: string, pass: string) {
-    if (!user || ! pass)
-      return
-    
-    this.webSocketService.send({
-      topic: 'user',
-      fn: 'login',
-      args: [user, pass]
-    })
+    if (user && pass)
+      this.webSocketService.send(new Command('user', 'login', [user, pass]))
   }
 
   public logout() {
-    this.webSocketService.send({
-      topic: 'user',
-      fn: 'logout',
-      args: [this.currentUser.id]
-    })
+    this.webSocketService.send(new Command('user', 'logout', [this.currentUser.id]))
     //this.requestAuth()
   }
 
   public register(user: string, pass: string) {
-    if (!user || ! pass)
-      return
-    
-    this.webSocketService.send({
-      topic: 'user',
-      fn: 'register',
-      args: [user, pass]
-    })
+    if (user && pass)
+      this.webSocketService.send(new Command('user', 'register', [user, pass]))
   }
 
   private loggedin(id: string, name: string, userlist?: User[]) {
-    this.currentUser = { id, name }
+    this.currentUser = NewUser(id, name);
     this.isLoggedIn = true
     if (userlist) {
       this.updateUserList(userlist)
@@ -67,29 +44,62 @@ export class UserService implements OnDestroy {
   }
 
   private updateUserList(userlist: User[]) {
-    this.allUsers = userlist
+    this.appStorage.users.forEach((user, key, users) => {
+      if (user.messages.length > 0) {
+        user.status = UserStatus.Disconnected
+      } else {
+        users.delete(key)
+      }
+    })
+
+    userlist.forEach(user => {
+      if (user.name !== this.currentUser.name) {
+        if (this.appStorage.users.has(name)) {
+          this.appStorage.users.get(name).status = UserStatus.Connected
+        } else {
+          this.appStorage.users.set(name, NewUser(user.id, user.name))
+        }
+      }
+    });
+
+    this.appStorage.notifyUsersChange()
   }
 
   private newUser(id: string, name: string) {
-    this.allUsers.push({ id, name })
+    this.appStorage.users.set(name, NewUser(id, name))
+    this.appStorage.notifyUsersChange()
   }
 
   private userConnect(id: string, name: string) {
-    this.allUsers.push({ id, name })
+    if (this.appStorage.users.has(name)) {
+      this.appStorage.users.get(name).status = UserStatus.Connected
+    } else {
+      this.appStorage.users.set(name, NewUser(id, name))
+    }
+    this.appStorage.notifyUsersChange()
   }
 
   private userDisconnect(id: string, name: string) {
-    this.allUsers = this.allUsers.filter(u => u.name !== name)
+    const disconnectedUser = this.appStorage.users.get(name)
+    if (disconnectedUser) {
+      if (disconnectedUser.messages.find(m => m.type === 'text')) {
+        disconnectedUser.status = UserStatus.Disconnected
+      } else {
+        this.appStorage.users.delete(name)
+      }
+      this.appStorage.notifyUsersChange()
+    }
   }
 
   public requestAuth() {
     this.isLoggedIn = false
     this.currentUser = undefined
-    this.allUsers = []
+    this.appStorage.users = new Map<string, User>()
     this.router.navigate(['login'])
+    this.appStorage.notifyUsersChange()
   }
 
   ngOnDestroy() {
-    this.socketSubscription.unsubscribe()
+    this.userSubscription.unsubscribe()
   }
 }
