d94be94fd10871b72b97e9f31fae258b030828d6
David Gonzalez <davidgg666@gmail.com>
2018-06-17 16:49:12 +0200

    Change User Interface

--- client/src/app/models/User.ts
+++ client/src/app/models/User.ts
@@ -1,4 +1,18 @@
+import { ChatMessage } from "./ChatMessage";
+
 export interface User {
   id:string
   name: string
+  status: UserStatus
+  messages: ChatMessage[]
+  unreadMessages: number
+}
+
+export enum UserStatus {
+  Disconnected,
+  Connected
+}
+
+export function NewUser(id: string, name: string) {
+  return { id, name, messages: [], status: UserStatus.Connected, unreadMessages: 0 }
 }
